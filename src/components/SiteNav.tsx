124488ba1e335b349b6c6e147f8db44d7e72deed
PeterSmart Link <petersmartlink@gmail.com>
2026-08-29 18:31:55 +0300

    web: expose developer platform in primary navigation

--- src/components/SiteNav.tsx
+++ src/components/SiteNav.tsx
@@ -6,6 +6,7 @@ import { usePathname } from 'next/navigation'
 
 const NAV_LINKS = [
   { label: 'OTYA', href: '/otya-player' },
+  { label: 'Developers', href: '/developers' },
   { label: 'Docs', href: '/docs' },
   { label: 'Support', href: '/apps/otya-player/support' },
 ]
