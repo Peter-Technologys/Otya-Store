c3aaf956be7eb72fe811e5adeb60015ab267c924
PeterSmart Link <petersmartlink@gmail.com>
2026-09-02 13:13:42 +0300

    feat(space): add dedicated security page

---
+++ src/app/account/security/page.tsx
@@ -0,0 +1,5 @@
+import { OtyaSpaceAccountSection } from '@/components/OtyaSpaceAccountSection'
+
+export default function SecurityPage() {
+  return <OtyaSpaceAccountSection section="security" />
+}
