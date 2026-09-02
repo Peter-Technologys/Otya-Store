69404d3c75da94d0f205ba552cb27db00dbea10d
PeterSmart Link <petersmartlink@gmail.com>
2026-09-02 13:14:15 +0300

    feat(space): add dedicated settings page

---
+++ src/app/account/settings/page.tsx
@@ -0,0 +1,5 @@
+import { OtyaSpaceAccountSection } from '@/components/OtyaSpaceAccountSection'
+
+export default function SettingsPage() {
+  return <OtyaSpaceAccountSection section="settings" />
+}
